5a4a5d8aa6672451fda739423561674480a18eb6
NiGereEpps <nigereepps@uchicago.edu>
2020-05-06 00:06:47 -0500

    Added user prompting
    Allows user to choose mode in the html page without having to download and edit the js file. Default values and error checking also implemented

--- js/AlgorithmicComposition.js
+++ js/AlgorithmicComposition.js
@@ -1,5 +1,4 @@
 function setup() {
-  //Inspired by Code Spirals made with Python's Turtle Graphics
   //By Ni'Gere Epps
   createCanvas(windowWidth, windowHeight);
   background('black');
@@ -25,10 +24,35 @@ function setup() {
     speed: Enter desired rotation speed
     space: Enter desired space between lines
   */
-  mode = 1
-  mousemode = 0
-  speed = 0.01
-  space = 2
+
+  mode = Number(prompt(`Select the mode that you want. The following are available:
+	1: (original) rainbow gradient spiral effect
+	2: random color spiral effect
+	3: black and white spiral effect
+	4: rainbow gradient pyramid / tunnel effect
+	5: random color pyramid / tunnel effect
+	6: black and white pyramid / tunnel effect`, 1))
+	
+  if(Number.isNaN(mode))
+    mode = 1
+  
+  mousemode = Number(prompt(`Choose if you want to control the center of the composition with your mouse:
+    0: disabled
+    1: enabled`, 0))
+	
+  if(Number.isNaN(mousemode))
+	mousemode = 0
+
+  speed = Number(prompt(`Enter desired rotation speed:`, 0.01))
+	
+  if(Number.isNaN(speed))
+	speed = 0.01
+
+  space = Number(prompt(`Enter desired space between lines (affects program speed):`, 2))
+	
+  if(Number.isNaN(space))
+	space = 2
+
 
   //Additional Variables 
   hueInc = 360/bound * space
--- js/main.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-  createCanvas(innerWidth, innerHeight)
-  background(0)
-  let x = width / 2
-  let y = height / 2
-  let r = width / 4
-  ellipse(x, y, r)
-}
